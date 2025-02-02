e4a9786bdb608b385553bafbbf8d6e949c5e8faf
stegmanta <tae@myfreeapps.com>
2025-02-01 22:01:57 -0700

    Finished PA01

--- SP2025-CIS340/src/PA01/TaylerStegmanPA0101.java
+++ SP2025-CIS340/src/PA01/TaylerStegmanPA0101.java
@@ -19,7 +19,7 @@ public class TaylerStegmanPA0101 {
 
 	public static void main(String[] args) {
 		// Declare variables and constants
-		int actReturnDay = 0, actReturnMo = 0, expReturnDay = 0, expReturnMo = 0, daysLate = 0, monthsLate = 0;
+		int actReturnDay = 0, actReturnMo = 0, expReturnDay = 0, expReturnMo = 0, daysLate = 0;
 		double fine = 0.0;
 		String errMsg = "";
 		boolean februaryErr = false,  aprilErr = false, inError = false;
@@ -35,40 +35,83 @@ public class TaylerStegmanPA0101 {
 			//check for generic errors (day greater than 31, month greater than 12, day/month <= 0
 			if(actReturnDay > 31 || expReturnDay > 31 || actReturnMo > 12 || expReturnMo > 12 
 					|| actReturnDay <= 0 || actReturnMo <= 0 || expReturnDay <=0 || expReturnMo <= 0 ) {
-				inError = true;
 				errMsg += "\nERROR: Ensure Day and Month input is valid, must be greater than 0";
+				throw new Exception();
 			} 
 			//check if february error is true
 			if(februaryErr) {
-				inError = true;
 				errMsg += "\nERROR: Febraury can't have more than 29 days";
+				throw new Exception();
 			}
 			//check if april error is true
 			if(aprilErr) {
-				inError = true;
 				errMsg += "\nERROR: April can't have more than 30 days";
-			}
-			//if in error is true throw exception
-			if(inError) 
 				throw new Exception();
-			
-			if(actReturnMo == expReturnMo){
-				//if returned in the same month
-				daysLate = Math.max(0, actReturnDay - expReturnDay);
-				fine = 0.15 *(daysLate);
-			}else if(actReturnMo > expReturnMo) {
-				//if returned in a different month
-				daysLate = (31 - expReturnDay) + actReturnDay + (actReturnMo - expReturnMo - 1) * 31;
-				fine = 5.00 * (actReturnMo - expReturnMo);
 			}
-			
-			JOptionPane.showMessageDialog(null, "Number of days late: " + daysLate +"\nFine Amount: $" +fine);
-			
 		}catch(Exception ex) {
 			JOptionPane.showMessageDialog(null, "Error:\nInputted Current Return Date (M/D): " + actReturnMo + "/" + actReturnDay 
 										 +"\nInputted Expected Return Date (M/D): " + expReturnMo + "/" + expReturnDay + errMsg);
 			System.exit(0);
-		}//end try
+		}//end try catch
+		
+		//switch to determine the max days given the month (would be good to put in a method but we're not that far yet in class)
+		int maxDays = 0;
+		switch(expReturnMo) {
+			case 2:
+				//years are not given so leap years can't be calculated
+				maxDays = 29;
+				break;
+			case 4:
+			case 6:
+			case 9:
+			case 11:
+				maxDays = 30;
+				break;
+			default:
+				maxDays = 31;
+				break;
+		}
+		
+		if (actReturnMo < expReturnMo || (actReturnMo == expReturnMo && actReturnDay <= expReturnDay)) {
+		    // Book returned on or before the expected date. fine is declared 0 so it is not nessecary
+		    daysLate = 0;
+		}
+		else if(actReturnMo == expReturnMo) {
+			//if month is the same calculate days late and fine
+			daysLate = actReturnDay - expReturnDay;
+			fine = 0.15 * daysLate;
+		}
+		else {
+			//if act return mo is greater than expected return mo
+			daysLate += (maxDays - expReturnDay);
+			//loop through total months and use switch to determine max
+			for(int m = expReturnMo + 1; m < actReturnMo; m++ ){
+				int daysInMonth = 0;
+				switch(m) {
+				case 2:
+					//not accounting for leap years (no input) but would be easy to implement ehre
+					daysInMonth = 29;
+					break;
+				case 4:
+				case 6:
+				case 9:
+				case 11:
+					daysInMonth = 30;
+					break;
+				default:
+					daysInMonth = 31;
+					break;
+				}
+				//add number of days late based on month that passed
+				daysLate += daysInMonth;
+			}//end for loop
+			//add days from the actual return month
+			daysLate += actReturnDay;
+			fine = 5.00 * (actReturnMo - expReturnMo);
+		}//end else
+		
+		JOptionPane.showMessageDialog(null, "Number of days late: " + daysLate +"\nFine Amount: $" + String.format("%.2f",fine));
+			
 	}//end main
 
 }
--- SP2025-CIS340/src/PA01/TaylerStegmanPA0102.java
+++ SP2025-CIS340/src/PA01/TaylerStegmanPA0102.java
@@ -19,46 +19,56 @@ public class TaylerStegmanPA0102 {
 	public static void main(String[] args) {
 		//declare variables and constants
 		double numBtc = 0.0, spendAmt = 0.0, numDollar = 0.0, usdForBtc = 0.0, 
-				btcForUsd =0.0, totalBtc = 0.0, amtUsd = 0.0;
+				btcForUsd =0.0, totalBtc = 0.0;
 		int transactionType;
-		String output = "";
+		String output = "", strErr = "";
 		final double BITCOIN_PRICE = 15000;
 		
 		try {
 			//get input for curr btc
 			numBtc = Double.parseDouble( JOptionPane.showInputDialog(null,"Number of bitcoins you own: "));
 			//if input less than one throw exception
-			if(numBtc < 1) 
+			if(numBtc < 1) {
+				strErr += "You must have atleast 1 BTC";
 				throw new Exception();
+			}
 			numDollar = numBtc * BITCOIN_PRICE;
 			//gather input for transaction type
 			transactionType = Integer.parseInt(JOptionPane.showInputDialog(null,"Choose a transaction type (0:Buy, 1: Sell): "));
 			//use switch statement to determine next input based on transaction type
 			switch(transactionType) {
 				case 0:
+					//BUY TRANSACTION
 					spendAmt = Double.parseDouble( JOptionPane.showInputDialog(null,"Amount you wish to spend on buying bitcoins: $"));
-					btcForUsd = spendAmt/BITCOIN_PRICE;
+					if (spendAmt < 100) {
+						strErr += "Minimum buying amount is $100";
+						throw new Exception();
+					}
+					btcForUsd = spendAmt / BITCOIN_PRICE;
 					totalBtc = numBtc + btcForUsd;
-					output += "\nDeposited Bitcoins for $" + String.format("%.0f", spendAmt) + ":   " + String.format("%.3f", btcForUsd)
+					output += "\nDeposited Bitcoins for $" + String.format("%,.0f", spendAmt) + ":   " + String.format("%.3f", btcForUsd)
 							  + "\nTotal Bitcoins:   " + String.format("%.3f",totalBtc)
-							  + "\nAmount in USD:   $" + String.format("%.0f",(totalBtc * BITCOIN_PRICE));
+							  + "\nAmount in USD:   $" + String.format("%,.0f",(totalBtc * BITCOIN_PRICE));
 					break;
 				case 1:
-					spendAmt = Double.parseDouble( JOptionPane.showInputDialog(null,"Amount of bitcoin you wish to sell: $"));
-					if(spendAmt > numDollar) 
+					//SELL TRANSACTION
+					spendAmt = Double.parseDouble( JOptionPane.showInputDialog(null,"Amount in USD you wish to withdraw by selling BTC: $"));
+					if(spendAmt > numDollar) {
+						strErr += "Withdrawal amount exceeds your current BTC balance in USD.";
 						throw new Exception();
-					else
+					}else {
 						usdForBtc = spendAmt/BITCOIN_PRICE;
 						totalBtc = numBtc - usdForBtc;
-						output += "\nWithdrawed Bitcoins for $" + String.format("%.0f", spendAmt) + ":   " + String.format("%.3f", btcForUsd)
+						output += "\nWithdrawed Bitcoins for $" + String.format("%,.0f", spendAmt) + ":   " + String.format("%.3f", usdForBtc)
 							   + "\nTotal Bitcoins:   " + String.format("%.3f",totalBtc)
-							   + "\nAmount in USD:   $" + String.format("%.0f",(totalBtc * BITCOIN_PRICE));
+							   + "\nAmount in USD:   $" + String.format("%,.0f",(totalBtc * BITCOIN_PRICE));
+					}
 					break;
 				default:
 					throw new Exception();
 			}
 		}catch(Exception ex) {
-			JOptionPane.showMessageDialog(null, "Error in program input");
+			JOptionPane.showMessageDialog(null, "Error in program input\nError: " + strErr);
 			System.exit(0);
 		}//end try catch
 		//display output
